07864d23965a9c1514366bc0e8840035085961ee
Joseph Calver <josephcalver@gmail.com>
2021-01-26 09:27:46 +0000

    Add resilience4j coverage on Companies Service

--- companies-service/src/main/java/com/josephcalver/companiesservice/services/CompaniesService.java
+++ companies-service/src/main/java/com/josephcalver/companiesservice/services/CompaniesService.java
@@ -3,8 +3,13 @@ package com.josephcalver.companiesservice.services;
 import com.josephcalver.companiesservice.exceptions.CompanyNotFoundException;
 import com.josephcalver.companiesservice.models.Company;
 import com.josephcalver.companiesservice.repositories.CompaniesRepository;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CompaniesService {
@@ -16,18 +21,40 @@ public class CompaniesService {
         this.companiesRepository = companiesRepository;
     }
 
+    @CircuitBreaker(name = "companiesService", fallbackMethod = "companiesDataUnavailable")
+    @Bulkhead(name = "bulkheadCompaniesService", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "companiesDataUnavailable")
     public Iterable<Company> getAllCompanies() {
         return companiesRepository.findAll();
     }
 
+    private Iterable<Company> companiesDataUnavailable() {
+        Company dummyCompany = new Company();
+        dummyCompany.setCompanyName("Companies data is currently unavailable");
+        List<Company> dummyList = new ArrayList<Company>();
+        dummyList.add(dummyCompany);
+        return dummyList;
+    }
+
+    @CircuitBreaker(name = "companiesService", fallbackMethod = "companyDataUnavailable")
+    @Bulkhead(name = "bulkheadCompaniesService", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "companyDataUnavailable")
     public Company getCompany(String companyId) {
         return companiesRepository.findByCompanyId(companyId);
     }
 
+    private Company companyDataUnavailable() {
+        Company dummy = new Company();
+        dummy.setCompanyName("Company data is currently unavailable");
+        return dummy;
+    }
+
+    @CircuitBreaker(name = "companiesService", fallbackMethod = "companyDataUnavailable")
+    @Bulkhead(name = "bulkheadCompaniesService", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "companyDataUnavailable")
     public Company createCompany(Company company) {
         return companiesRepository.save(company);
     }
 
+    @CircuitBreaker(name = "companiesService", fallbackMethod = "companyDataUnavailable")
+    @Bulkhead(name = "bulkheadCompaniesService", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "companyDataUnavailable")
     public Company updateCompany(String companyId, Company company) {
 
         Company existingCompany = companiesRepository.findByCompanyId(companyId);
@@ -46,7 +73,10 @@ public class CompaniesService {
         return companiesRepository.save(existingCompany);
     }
 
+    @CircuitBreaker(name = "companiesService")
+    @Bulkhead(name = "bulkheadCompaniesService", type = Bulkhead.Type.THREADPOOL)
     public void deleteCompany(String companyId) {
         companiesRepository.deleteByCompanyId(companyId);
     }
+
 }
